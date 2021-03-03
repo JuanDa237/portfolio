5c87e59867e9ff99eb6350dc30859bc5b60f99c7
JuanDa237 <juandavid200301@hotmail.com>
2021-03-03 16:29:46 -0500

    - Change btn to toggle sidenav

--- frontend/src/app/containers/main/main.component.ts
+++ frontend/src/app/containers/main/main.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
 	selector: 'app-main',
 	templateUrl: './main.component.html',
 	styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
-	constructor(private navigationService: NavigationService) {}
-
-	public toggleSideNav(): void {
-		this.navigationService.toggleSidenav();
-	}
-}
+export class MainComponent {}
--- frontend/src/app/containers/side-nav/side-nav.component.ts
+++ frontend/src/app/containers/side-nav/side-nav.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { NavigationService } from 'src/app/services/navigation.service';
 
 @Component({
 	selector: 'app-side-nav',
 	templateUrl: './side-nav.component.html',
 	styleUrls: ['./side-nav.component.scss']
 })
-export class SideNavComponent {}
+export class SideNavComponent {
+	constructor(private navigationService: NavigationService) {}
+
+	public toggleSideNav(): void {
+		this.navigationService.toggleSidenav();
+	}
+}
