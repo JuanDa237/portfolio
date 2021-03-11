956c9c44d58550972f68bf6dac6ad67a295f4f62
JuanDa237 <juandavid200301@hotmail.com>
2021-03-10 22:56:29 -0500

    - Add progress bar in skills

--- frontend/src/app/components/index.ts
+++ frontend/src/app/components/index.ts
@@ -1,5 +1,11 @@
 import { SectionComponent } from './section/section.component';
 import { ServicesOneComponent } from './services-one/services-one.component';
 import { ServicesTwoComponent } from './services-two/services-two.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
-export const components = [SectionComponent, ServicesOneComponent, ServicesTwoComponent];
+export const components = [
+	SectionComponent,
+	ServicesOneComponent,
+	ServicesTwoComponent,
+	ProgressBarComponent
+];
