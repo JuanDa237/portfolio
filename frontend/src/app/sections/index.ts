2d501340663167d3649249e001af5282ce35fc28
JuanDa237 <juandavid200301@hotmail.com>
2021-03-11 17:53:32 -0500

    - New education info

--- frontend/src/app/sections/index.ts
+++ frontend/src/app/sections/index.ts
@@ -2,7 +2,6 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { EducationComponent } from './education/education.component';
 import { ExperienceComponent } from './experience/experience.component';
-import { ServicesComponent } from './services/services.component';
 import { SkillsComponent } from './skills/skills.component';
 import { WorkComponent } from './work/work.component';
 import { HomeComponent } from './home/home.component';
@@ -12,7 +11,6 @@ export const sections = [
 	ContactComponent,
 	EducationComponent,
 	ExperienceComponent,
-	ServicesComponent,
 	SkillsComponent,
 	WorkComponent,
 	HomeComponent
