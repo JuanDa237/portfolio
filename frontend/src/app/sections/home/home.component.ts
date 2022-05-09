a4d1d926fc003655c7d6662a8fa0adb0734f4c10
JuanDa237 <juandavid200301@hotmail.com>
2022-05-09 04:41:27 -0500

    - Home section translation implemented

--- frontend/src/app/sections/home/home.component.ts
+++ frontend/src/app/sections/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { interval, take } from 'rxjs';
 
 @Component({
@@ -7,19 +8,28 @@ import { interval, take } from 'rxjs';
 	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-	constructor() {}
+	private translating: 'not-used' | 'running' | 'used';
+
+	constructor(private translate: TranslateService) {
+		this.translating = 'not-used';
+	}
 
 	ngOnInit(): void {
-		this.typeAnimation();
+		this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+			if (this.translating == 'not-used') this.typeAnimation();
+			else if (this.translating == 'used') this.translateAfterAnimation();
+		});
 	}
 
 	private typeAnimation() {
+		this.translating = 'running';
+
 		let tag = document.getElementById('text');
 
 		if (!tag) return;
 
 		tag.innerHTML = '';
-		let text = "Hello\nI'm\nJuan";
+		let text = this.translate.instant('home.hello');
 		let index = 0;
 		const timer = interval(250).pipe(take(text.length));
 
@@ -36,6 +46,8 @@ export class HomeComponent implements OnInit {
 				index++;
 			})
 			.add(() => {
+				this.translating = 'used';
+
 				window.requestAnimationFrame(() => {
 					let tag2 = document.getElementById('subtitle');
 					if (!tag2) return;
@@ -43,5 +55,21 @@ export class HomeComponent implements OnInit {
 					tag2.className = 'fade-in';
 				});
 			});
+	}
+
+	private translateAfterAnimation() {
+		let tag = document.getElementById('text');
+		if (!tag) return;
+
+		let text = this.translate.instant('home.hello');
+		tag.innerText = '';
+
+		for (let index = 0; index < text.length; index++) {
+			if (text.charAt(index) == '\n') {
+				tag.innerHTML += '<br/>';
+			} else {
+				tag.innerHTML += text.charAt(index);
+			}
+		}
 	}
 }
