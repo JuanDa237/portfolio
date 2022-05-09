905b599e5347e39247e0daa3c9009d11838a58bd
JuanDa237 <juandavid200301@hotmail.com>
2022-05-09 17:18:08 -0500

    - Create experience and contact sections

--- frontend/src/app/sections/index.ts
+++ frontend/src/app/sections/index.ts
@@ -1,7 +1,11 @@
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const sections = [HomeComponent, AboutMeComponent];
+export const sections = [HomeComponent, AboutMeComponent, ExperienceComponent, ContactComponent];
 
 export * from './home/home.component';
 export * from './about-me/about-me.component';
+export * from './experience/experience.component';
+export * from './contact/contact.component';
